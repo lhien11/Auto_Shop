f3e6e3d3ae37471dc3b1ec86d43e7885ef148f60
Hien Le <hienle@Hiens-MacBook-Pro.local>
2017-05-13 04:09:45 -0500

    addFeature

---
+++ Persistence/VegaDbContext.cs
@@ -0,0 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using vega.Models;
+
+namespace vega.Persistence
+{
+    public class VegaDbContext : DbContext
+    {
+
+        public VegaDbContext(DbContextOptions<VegaDbContext> options)
+            :base(options)
+        {
+            //System.Configuration.ConfigurationManager
+        }
+        public DbSet<Make> Makes { get; set; }
+        public DbSet<Feature> Features { get; set; }
+
+        
+    }
+}
